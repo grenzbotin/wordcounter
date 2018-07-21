28d973474e42df178a3fe1e678c925a53e5c804c
grenzbotin <grenzbotin@gmail.com>
2018-07-21 21:17:30 +0200

    german language: correct writing of umlauts

--- wordcounter/word_counter.py
+++ wordcounter/word_counter.py
@@ -1,9 +1,12 @@
-import sys,re,collections,os,json
+#!/usr/bin/env python
+# coding: utf8
+
+import sys,re,collections,io,os,json
 import treetaggerwrapper
 
 from os.path import join, dirname
 from dotenv import load_dotenv
- 
+
 # Create .env file path.
 dotenv_path = join(dirname(__file__), '..', '.env')
  
@@ -12,7 +15,7 @@ load_dotenv(dotenv_path)
 
 # patterns that used to find or/and replace particular chars or words
 # to find chars that are not a letter, a blank or a quotation
-pat_letter = re.compile(r'[^a-zA-Z \']+')
+pat_letter = re.compile('[^a-zA-ZüöäÜÖÄ \']+')
 # to find the 's following the pronouns. re.I is refers to ignore case
 pat_is = re.compile("(it|he|she|that|this|there|here)(\'s)", re.I)
 # to find the 's following the letters
@@ -20,7 +23,7 @@ pat_s = re.compile("(?<=[a-zA-Z])\'s")
 # to find the ' following the words ending by s
 pat_s2 = re.compile("(?<=s)\'s?")
 # to find the abbreviation of not
-pat_not = re.compile("(?<=[a-zA-Z])n\'t")
+pat_not = re.compile("(?<=[a-zA-])n\'t")
 # to find the abbreviation of would
 pat_would = re.compile("(?<=[a-zA-Z])\'d")
 # to find the abbreviation of will
@@ -34,28 +37,33 @@ pat_ve = re.compile("(?<=[a-zA-Z])\'ve")
 
 path = 'files'
 
-def get_words(file):  
-    with open (path+'/'+file) as f:  
+def get_words(file, language):
+    with io.open(path+'/'+file,"r") as f:  
         words_box = []
-        pat = re.compile(r'[^a-zA-Z \']+')
-        for line in f:                           
-            words_box.extend(replace_abbreviations(line).split())
+        for line in f:
+            words_box.extend(replace_abbreviations(line, language).split())
     return collections.Counter(words_box)
 
-def replace_abbreviations(text):
+def replace_abbreviations(text, language):
     new_text = text
-    new_text = pat_letter.sub(' ', text).strip().lower()
-    new_text = pat_is.sub(r"\1 is", new_text)
-    new_text = pat_s.sub("", new_text)
-    new_text = pat_s2.sub("", new_text)
-    new_text = pat_not.sub(" not", new_text)
-    new_text = pat_would.sub(" would", new_text)
-    new_text = pat_will.sub(" will", new_text)
-    new_text = pat_am.sub(" am", new_text)
-    new_text = pat_are.sub(" are", new_text)
-    new_text = pat_ve.sub(" have", new_text)
-    new_text = new_text.replace('\'', ' ')
-    return new_text
+
+    if language == "de":
+        new_text = re.sub(r"[0-9-()\"#/@;:<>{}\[\]`+=~|.!?,]", '', text)
+
+    elif language == "en":
+        new_text = pat_letter.sub(' ', text).strip().lower()
+        new_text = pat_is.sub(r"\1 is", new_text)
+        new_text = pat_s.sub("", new_text)
+        new_text = pat_s2.sub("", new_text)
+        new_text = pat_not.sub(" not", new_text)
+        new_text = pat_would.sub(" would", new_text)
+        new_text = pat_will.sub(" will", new_text)
+        new_text = pat_am.sub(" am", new_text)
+        new_text = pat_are.sub(" are", new_text)
+        new_text = pat_ve.sub(" have", new_text)
+        new_text = new_text.replace('\'', ' ')
+
+    return new_text.lower()
 
 def write_to_json(words, data, destination, tagger):
     if len(destination) > 0:
@@ -104,7 +112,7 @@ if __name__=='__main__':
     for filename in os.listdir(path):
         if filename.endswith('.txt'):
             print "counting..."
-            words = get_words(filename)
+            words = get_words(filename, language)
 
             if option == "filename":
                 destination = filename.split("_")
@@ -112,7 +120,11 @@ if __name__=='__main__':
             print "writing file..."
             write_to_json(words.most_common(), data, destination, tagger)
 
-    with open('data.json', 'w') as outfile:  
-        json.dump(data, outfile)
+    with io.open('data.json', 'w',encoding="utf-8") as outfile:
+        my_json_str = json.dumps(data, ensure_ascii=False)
+        if isinstance(my_json_str, str):
+            my_json_str = my_json_str.decode("utf-8")
+
+        outfile.write(my_json_str)
     
 
